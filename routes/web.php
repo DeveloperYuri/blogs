870db5dd159ec2a11db30ad1b0334b31e3a9937e
DeveloperYuri <developeryuri2@gmail.com>
2025-02-06 15:30:39 +0700

    update add template admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,7 @@ Route::post('forgot-password', [AuthController::class, 'forgot_password']);
 
 Route::get('reset/{token}', [AuthController::class, 'reset']);
 Route::post('reset/{token}', [AuthController::class, 'post_reset']);
+
+Route::get('panel/dashboard', [DashboardController::class, 'dashboard']);
+
                                                   
