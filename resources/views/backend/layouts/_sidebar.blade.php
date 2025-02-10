2686a029e37a5ddc14b6ef5b98d592d4eb368484
DeveloperYuri <developeryuri2@gmail.com>
2025-02-10 13:58:04 +0700

    update sidebar active

--- resources/views/backend/layouts/_sidebar.blade.php
+++ resources/views/backend/layouts/_sidebar.blade.php
@@ -4,14 +4,14 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="{{ url('panel/dashboard')}}">
+        <a class="nav-link @if(Request::segment(2) != 'dashboard') collapsed @endif" href="{{ url('panel/dashboard')}}">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('panel/user/list')}}">
+        <a class="nav-link @if(Request::segment(2) != 'user') collapsed @endif" href="{{ url('panel/user/list')}}">
           <i class="bi bi-person"></i>
           <span>User</span>
         </a>
