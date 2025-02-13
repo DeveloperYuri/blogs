76dfb7e0fdd8851652ab80f98fd2329524f8a64c
DeveloperYuri <developeryuri2@gmail.com>
2025-02-13 09:59:54 +0700

    update filter searching

--- app/Models/BlogModel.php
+++ app/Models/BlogModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Request;
 
 class BlogModel extends Model
 {
@@ -17,12 +18,69 @@ class BlogModel extends Model
     }
 
     static public function getRecord(){
-        return self::select('blog.*', 'users.name as user_name', 'category.name as category_name' )
+        $return = self::select('blog.*', 'users.name as user_name', 'category.name as category_name' )
         ->join('users', 'users.id', '=', 'blog.user_id')
-        ->join('category', 'category.id', '=', 'blog.category_id')
-        ->where('blog.is_delete', '=', 0)
+        ->join('category', 'category.id', '=', 'blog.category_id');
+
+        if (!empty(Request::get('id'))) 
+        {
+            $return = $return->where('blog.id', '=', Request::get('id'));
+        }
+
+        if (!empty(Request::get('username'))) 
+        {
+            $return = $return->where('users.name', 'like', '%'.Request::get('username').'%');
+        }
+
+        if (!empty(Request::get('title'))) 
+        {
+            $return = $return->where('blog.title', 'like', '%'.Request::get('title').'%');
+        }
+
+        if (!empty(Request::get('category'))) 
+        {
+            $return = $return->where('category.name', 'like', '%'.Request::get('category').'%');
+        }
+
+        if(!empty(Request::get('is_publish')))
+        {
+            $is_publish = Request::get('is_publish');
+
+            if($is_publish == 100)
+            {
+                $is_publish = 0;
+            }
+            
+            $return = $return->where('blog.is_publish', '=', $is_publish);
+        }
+
+        if(!empty(Request::get('status')))
+        {
+            $status = Request::get('status');
+
+            if($status == 100)
+            {
+                $status = 0;
+            }
+            
+            $return = $return->where('blog.status', '=', $status);
+        }
+
+        if (!empty(Request::get('start_date'))) 
+        {
+            $return = $return->whereDate('blog.created_at', '>=', Request::get('start_date'));
+        }
+
+        if (!empty(Request::get('end_date'))) 
+        {
+            $return = $return->whereDate('blog.created_at', '<=', Request::get('end_date'));
+        }
+
+        $return = $return->where('blog.is_delete', '=', 0)
         ->orderBy('blog.id', 'desc')
         ->paginate(20);
+
+        return $return;
     }
 
     public function getImage(){
--- resources/views/backend/blog/list.blade.php
+++ resources/views/backend/blog/list.blade.php
@@ -13,9 +13,61 @@
                 <div class="card">
 
                     <div class="card-body">
-                        <h5 class="card-title">Blog List
-                            <a href="{{ url('panel/blog/add')}}" class="btn btn-primary" style="float: right; margin-top:-12px">Add New</a>
+                        <h5 class="card-title">Blog List (Total : {{ $getRecord->total() }})
+                            <a href="{{ url('panel/blog/add') }}" class="btn btn-primary"
+                                style="float: right; margin-top:-12px">Add New</a>
                         </h5>
+
+                        <form class="row g-3" accept="get">
+                            <div class="col-md-1" style="margin-bottom: 10px">
+                                <label for="inputNanme4" class="form-label">ID</label>
+                                <input type="text" name="id" value="{{ Request::get('id') }}" class="form-control" id="inputNanme4">
+                            </div>
+                            <div class="col-md-2" style="margin-bottom: 10px">
+                                <label for="inputNanme4" class="form-label">Username</label>
+                                <input type="text" name="username" value="{{ Request::get('username') }}" class="form-control" id="inputNanme4">
+                            </div>
+                            <div class="col-md-3" style="margin-bottom: 10px">
+                                <label for="inputNanme4" class="form-label">Title</label>
+                                <input type="text" name="title" value="{{ Request::get('title') }}" class="form-control" id="inputNanme4">
+                            </div>
+                            <div class="col-md-2" style="margin-bottom: 10px">
+                                <label for="inputNanme4" class="form-label">Category</label>
+                                <input type="text" name="category" value="{{ Request::get('category') }}" class="form-control" id="inputNanme4">
+                            </div>
+                            <div class="col-md-2" style="margin-bottom: 10px">
+                                <label for="inputNanme4" class="form-label">Publish</label>
+                                <select class="form-control" name="is_publish">
+                                    <option value="">Select</option>
+                                    <option {{ (Request::get('is_publish') == 1) ? 'selected' : '' }} value="1">Yes</option>
+                                    <option {{ (Request::get('is_publish') == 100) ? 'selected' : '' }} value="100">No</option>
+                                </select>
+                            </div>
+                            <div class="col-md-2" style="margin-bottom: 10px">
+                                <label for="inputNanme4" class="form-label">Status</label>
+                                <select class="form-control" name="status">
+                                    <option value="">Select</option>
+                                    <option {{ (Request::get('status') == 1) ? 'selected' : '' }} value="1">Active</option>
+                                    <option {{ (Request::get('status') == 100) ? 'selected' : '' }} value="100">InActive</option>
+                                </select>
+                            </div>
+                            <div class="col-md-2" style="margin-bottom: 10px">
+                                <label for="inputNanme4" class="form-label">Start Date</label>
+                                <input type="date" name="start_date" value="{{ Request::get('start_date') }}" class="form-control" id="inputNanme4">
+                            </div>
+                            <div class="col-md-2" style="margin-bottom: 10px">
+                                <label for="inputNanme4" class="form-label">End Date</label>
+                                <input type="date" name="end_date" value="{{ Request::get('end_date') }}" class="form-control" id="inputNanme4">
+                            </div>
+                            
+                            <div class="col-md-2">
+                                <label class="form-label" style="display: block">&nbsp;</label>
+                                <button type="submit" class="btn btn-primary">Search</button>
+                                <a href={{ url('panel/blog/list') }} class="btn btn-secondary">Reset</a>
+                            </div>
+                        </form>
+
+                        <hr>
 
                         <!-- Table with stripped rows -->
                         <table class="table table-striped">
@@ -36,26 +88,30 @@
                             </thead>
                             <tbody>
                                 @forelse ($getRecord as $value)
-                                <tr>
-                                    <th scope="row">{{ $value->id }}</th>
-                                    <td>
-                                        @if (!empty($value->getImage()))
-                                        <img src="{{ $value->getImage() }}" alt="" style="height: 100px; width:100px">
-                                        @endif
-                                    </td>
-                                    <td>{{ $value->user_name }}</td>
-                                    <td>{{ $value->title }}</td>
-                                    <td>{{ $value->category_name }}</td>
-                                    <td>{{ $value->meta_description }}</td>
-                                    <td>{{ $value->meta_keywords }}</td>
-                                    <td>{{ !empty($value->status) ? 'Active' : 'InActive' }}</td>
-                                    <td>{{ !empty($value->is_publish) ? 'Yes' : 'No' }}</td>
-                                    <td>{{ date('d-m-Y H:i A', strtotime($value->created_at))  }}</td>
-                                    <td>
-                                        <a href="{{ url('panel/blog/edit/'.$value->id)}}" class="btn btn-primary btn-sm">Edit</a>
-                                        <a onclick="return confirm('Are you sure you want delete record?')" href="{{ url('panel/blog/delete/'.$value->id)}}" class="btn btn-danger btn-sm">Delete</a>
-                                    </td>
-                                </tr>
+                                    <tr>
+                                        <th scope="row">{{ $value->id }}</th>
+                                        <td>
+                                            @if (!empty($value->getImage()))
+                                                <img src="{{ $value->getImage() }}" alt=""
+                                                    style="height: 100px; width:100px">
+                                            @endif
+                                        </td>
+                                        <td>{{ $value->user_name }}</td>
+                                        <td>{{ $value->title }}</td>
+                                        <td>{{ $value->category_name }}</td>
+                                        <td>{{ $value->meta_description }}</td>
+                                        <td>{{ $value->meta_keywords }}</td>
+                                        <td>{{ !empty($value->status) ? 'Active' : 'InActive' }}</td>
+                                        <td>{{ !empty($value->is_publish) ? 'Yes' : 'No' }}</td>
+                                        <td>{{ date('d-m-Y H:i A', strtotime($value->created_at)) }}</td>
+                                        <td>
+                                            <a href="{{ url('panel/blog/edit/' . $value->id) }}"
+                                                class="btn btn-primary btn-sm">Edit</a>
+                                            <a onclick="return confirm('Are you sure you want delete record?')"
+                                                href="{{ url('panel/blog/delete/' . $value->id) }}"
+                                                class="btn btn-danger btn-sm">Delete</a>
+                                        </td>
+                                    </tr>
                                 @empty
                                     <tr>
                                         <td colspan="100%">Record Not Found</td>
